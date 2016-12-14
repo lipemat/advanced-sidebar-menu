017a76e7b62fbf9f5458c326bb54d4244b3bf7f3
lipemat <mat@matlipe.com>
2016-12-14 11:04:12 -0500

    bump to version 6.1.5

--- advanced-sidebar-menu.php
+++ advanced-sidebar-menu.php
@@ -4,12 +4,12 @@ Plugin Name: Advanced Sidebar Menu
 Plugin URI: https://matlipe.com/advanced-sidebar-menu/
 Description: Creates dynamic menus based on parent/child relationship of your pages or categories.
 Author: Mat Lipe
-Version: 6.1.4
+Version: 6.1.5
 Author URI: https://matlipe.com
 Text Domain: advanced-sidebar-menu
 */
 
-define( 'ADVANCED_SIDEBAR_BASIC_VERSION', '6.1.4' );
+define( 'ADVANCED_SIDEBAR_BASIC_VERSION', '6.1.5' );
 define( 'ADVANCED_SIDEBAR_DIR', plugin_dir_path( __FILE__ ) );
 
 if( !function_exists( 'advanced_sidebar_menu_load' ) ){
--- readme.txt
+++ readme.txt
@@ -4,8 +4,8 @@ Contributors: Mat Lipe
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=paypal%40matlipe%2ecom&lc=US&item_name=Advanced%20Sidebar%20Menu&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest
 Tags: menus, sidebar menu, hierarchy, category menu, pages menu
 Requires at least: 4.4.0
-Tested up to: 4.6.1
-Stable tag: 6.1.4
+Tested up to: 4.7.0
+Stable tag: 6.1.5
 
 == Description ==
 
