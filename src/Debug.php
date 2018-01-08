1b8744baf5f786d8da7db3a9a32d68c82b98ca50
lipemat <mat@matlipe.com>
2018-01-08 14:19:50 -0500

    add pro_version to asm_debug variable

--- src/Debug.php
+++ src/Debug.php
@@ -11,8 +11,9 @@
 class Advanced_Sidebar_Menu_Debug {
 	const DEBUG_PARAM = 'asm_debug';
 
+
 	protected function hook() {
-		if( !empty( $_REQUEST[ self::DEBUG_PARAM ] ) ){
+		if ( ! empty( $_GET[ self::DEBUG_PARAM ] ) ) {
 			add_action( 'advanced_sidebar_menu_widget_pre_render', array( $this, 'print_instance' ), 1, 2 );
 		}
 	}
@@ -20,19 +21,35 @@ class Advanced_Sidebar_Menu_Debug {
 
 	/**
 	 *
-	 * @param Advanced_Sidebar_Menu_Menu $asm
-	 * @param \advanced_sidebar_menu_page $widget
+	 * @param Advanced_Sidebar_Menu_Menu        $asm
+	 * @param Advanced_Sidebar_Menu_Widget_Page $widget
 	 *
 	 * @return void
 	 */
-	public function print_instance( $asm, $widget ){
-		?>
-		<script class="<?php echo  self::DEBUG_PARAM; ?>">
-			var <?php echo  self::DEBUG_PARAM; ?> = window.<?php echo  self::DEBUG_PARAM; ?> || {};
-			    <?php echo  self::DEBUG_PARAM; ?>['version'] = '<?php echo  ADVANCED_SIDEBAR_BASIC_VERSION; ?>';
-                <?php echo  self::DEBUG_PARAM; ?>['<?php echo  $widget->id; ?>'] = <?php echo  json_encode( $asm->instance ); ?>;
-		</script>
-		<?php
+	public function print_instance( $asm, $widget ) {
+		static $printed = false;
+		$data = array(
+			'version'   => ADVANCED_SIDEBAR_BASIC_VERSION,
+			$widget->id => $asm->instance,
+		);
+		if ( defined( 'ADVANCED_SIDEBAR_MENU_PRO_VERSION' ) ) {
+			$data['pro_version'] = ADVANCED_SIDEBAR_MENU_PRO_VERSION;
+		}
+
+		if ( ! $printed ) {
+			?>
+			<script class="<?php echo esc_attr( self::DEBUG_PARAM ); ?>">
+				var <?php echo esc_attr( self::DEBUG_PARAM ); ?> = <?php echo wp_json_encode( $data ); ?>;
+			</script>
+			<?php
+			$printed = true;
+		} else {
+			?>
+			<script class="<?php echo esc_attr( self::DEBUG_PARAM ); ?>">
+					<?php echo esc_attr( self::DEBUG_PARAM ); ?>['<?php echo esc_js( $widget->id ); ?>'] = <?php echo wp_json_encode( $asm->instance ); ?>;
+			</script>
+			<?php
+		}
 	}
 
 	//********** SINGLETON **********/
@@ -63,7 +80,7 @@ class Advanced_Sidebar_Menu_Debug {
 	 * @return self
 	 */
 	public static function instance() {
-		if( !is_a( self::$instance, __CLASS__ ) ){
+		if ( ! is_a( self::$instance, __CLASS__ ) ) {
 			self::$instance = new self();
 		}
 
