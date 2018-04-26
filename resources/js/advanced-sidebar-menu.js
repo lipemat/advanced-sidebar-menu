fc63786d80c1301f34ad4a4ffe1284513b212953
lipemat <mat@matlipe.com>
2018-04-26 12:46:54 -0400

    remove superfluous console.log

--- resources/js/advanced-sidebar-menu.js
+++ resources/js/advanced-sidebar-menu.js
@@ -8,7 +8,6 @@ function asm_reveal_element(this_element_id) {
 	//old used ids. New use data-js so multiple may be used specifically
 	//@todo remove this select of id once PRO has been converted over
 	var el = jQuery( '[id="' + this_element_id + '"]:first');
-	console.info( el.length );
 	if( ! el.length ){
 		el = jQuery( '[data-js="' + this_element_id + '"]' );
 	}
