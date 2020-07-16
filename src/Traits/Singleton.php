9672cde55e4543186fdd0d36cd67abdc447e76f9
lipemat <mat@matlipe.com>
2020-07-16 09:11:50 -0600

    Support calling init_once via Singleton traits

--- src/Traits/Singleton.php
+++ src/Traits/Singleton.php
@@ -17,6 +17,15 @@ trait Singleton {
 	 */
 	protected static $instance;
 
+	/**
+	 * Tracks if init has been called.
+	 *
+	 * @since 8.0.1
+	 *
+	 * @var bool
+	 */
+	protected static $inited = false;
+
 
 	/**
 	 * Create the instance of the class
@@ -28,6 +37,24 @@ trait Singleton {
 		static::$instance = static::instance();
 		if ( method_exists( static::$instance, 'hook' ) ) {
 			static::$instance->hook();
+		}
+		static::$inited = true;
+	}
+
+
+	/**
+	 * Call this method as many times as needed and the
+	 * class will only init() one time.
+	 *
+	 * @static
+	 *
+	 * @since 8.0.1
+	 *
+	 * @return void
+	 */
+	public static function init_once() {
+		if ( ! static::$inited ) {
+			static::init();
 		}
 	}
 
