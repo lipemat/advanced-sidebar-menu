aa665d2b7ad889ce9f83677c3430fb418c10dbac
lipemat <mat@matlipe.com>
2021-03-30 11:50:41 -0400

    Update "Requires at least" in main plugin file to match readme

--- advanced-sidebar-menu.php
+++ advanced-sidebar-menu.php
@@ -9,7 +9,7 @@
  * Text Domain: advanced-sidebar-menu
  * Domain Path: /languages/
  * Network: false
- * Requires at least: 5.0
+ * Requires at least: 5.2
  * Requires PHP: 5.6.0
  *
  * @package advanced-sidebar-menu
