f607620d01e8eaaaf8e693692ac4346ee86879ea
lipemat <mat@matlipe.com>
2022-07-08 12:20:58 -0400

    Run `start` script on basic version on port 3001
    Allows us to run both plugins' `start` script at the same time.
    
    [#625625414772799]

---
+++ js/config/dev-server.config.js
@@ -0,0 +1,9 @@
+/**
+ * Use a separate port for basic version, so we can run both plugin's
+ * `start` script at the same time.
+ *
+ * @type {{post: number}}
+ */
+module.exports = {
+	port: 3001,
+};
