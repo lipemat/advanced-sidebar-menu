1ac4d0c5d64d296db8b4cdbab9336672edbf51ec
lipemat <mat@matlipe.com>
2022-07-18 14:19:42 -0400

    Create a block example when hovering the block in the blocks list
    Create some default attributes to send for basic and PRO versions of the
     block to generate a preview based on the current page.
    
    Using OnPoint Plugin as an example for settings.
    
    [#625625414772799]

--- js/src/gutenberg/blocks/pages/block.tsx
+++ js/src/gutenberg/blocks/pages/block.tsx
@@ -30,6 +30,35 @@ export type setAttributes = ( newValue: {
 	[attribute in keyof Attr]?: Attr[attribute]
 } ) => void;
 
+/**
+ * Attributes used for the example preview.
+ * Combines some PRO and basic attributes.
+ * The PRO attributes will only be sent if PRO is active.
+ */
+const example = {
+	include_parent: true,
+	include_childless_parent: true,
+	display_all: true,
+	levels: '2',
+	apply_current_page_styles_to_parent: true,
+	apply_current_page_parent_styles_to_parent: true,
+	block_style: true,
+	border: true,
+	border_color: '#333',
+	bullet_style: 'none',
+	parent_page_color: '#fff',
+	parent_page_bg_color: '#666',
+	child_page_color: '#fff',
+	child_page_bg_color: '#666',
+	grandchild_page_color: '#282828',
+	grandchild_page_bg_color: '#989898',
+	grandchild_page_font_weight: 'bold',
+	current_page_color: '#0cc4c6',
+	current_page_bg_color: '#282828',
+	current_page_font_weight: 'normal',
+	current_page_parent_bg_color: '#333',
+};
+
 export const block = CONFIG.blocks.pages;
 
 export const name = block.id;
@@ -49,6 +78,9 @@ export const settings: BlockSettings<Attr> = {
 		'sidebar',
 		'pages',
 	],
+	example: {
+		attributes: example as any,
+	},
 	// `attributes` are registered server side because we use ServerSideRender.
 	// `supports` are registered server side for easy overrides.
 	edit: props => (
