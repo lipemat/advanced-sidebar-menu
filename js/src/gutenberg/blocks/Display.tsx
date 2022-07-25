2d06cd24e22d43aa5af13a25e261a0202270b6aa
lipemat <mat@matlipe.com>
2022-07-25 13:56:08 -0400

    Separate Slots used in Display component based on widget id
    Introduce `AdvancedSidebarMenuCategoriesDisplay` slot
    
    [#625625414772799]

--- js/src/gutenberg/blocks/Display.tsx
+++ js/src/gutenberg/blocks/Display.tsx
@@ -1,21 +1,28 @@
 import {CheckboxControl, PanelBody, Slot} from '@wordpress/components';
 import {CONFIG, I18N} from '../../globals/config';
-import type {Attr, setAttributes} from './pages/block';
+import type {setAttributes} from './pages/block';
 import {sprintf} from '@wordpress/i18n';
 import {Type} from '@wordpress/api/types';
 import {range} from 'lodash';
 import reactStringReplace from 'react-string-replace';
 import {Taxonomy} from '@wordpress/api/taxonomies';
 
+export type DisplayOptions = {
+	display_all: boolean;
+	include_childless_parent: boolean;
+	include_parent: boolean;
+	levels: string;
+}
 
 type Props = {
-	attributes: Attr;
+	attributes: DisplayOptions;
 	setAttributes: setAttributes;
 	type?: Type | Taxonomy;
+	name: string;
 };
 
 
-const checkboxes: { [attr in keyof Partial<Attr>]: string } = {
+const checkboxes: { [attr in keyof Partial<DisplayOptions>]: string } = {
 	include_parent: I18N.display.highest,
 	include_childless_parent: I18N.display.childless,
 	display_all: I18N.display.always,
@@ -30,7 +37,9 @@ const checkboxes: { [attr in keyof Partial<Attr>]: string } = {
  * 5. Display levels of child pages.
  *
  */
-const Display = ( {attributes, setAttributes, type}: Props ) => {
+const Display = ( {attributes, setAttributes, type, name}: Props ) => {
+	const showLevels = ( CONFIG.blocks.pages.id === name && CONFIG.isPro ) || attributes.display_all;
+
 	return (
 		<PanelBody
 			title={I18N.display.title}
@@ -52,24 +61,26 @@ const Display = ( {attributes, setAttributes, type}: Props ) => {
 					}}
 				/>;
 			} )}
-			{( CONFIG.isPro || attributes.display_all ) &&
-				reactStringReplace( I18N.display.levels.replace( '%2$s', type?.labels?.name.toLowerCase() ?? '' ), '%1$s',
-					() => (
-						<select
-							key={'levels'}
-							value={attributes.levels}
-							onChange={ev => setAttributes( {levels: ev.target.value} )}
-						>
-							<option value="100">
-								{I18N.display.all}
-							</option>
-							{range( 1, 10 ).map( n => <option
-								key={n}
-								value={n}>{n}</option> )}
-						</select>
-					) )}
+			{showLevels && reactStringReplace( I18N.display.levels.replace( '%2$s', type?.labels?.name.toLowerCase() ?? '' ), '%1$s',
+				() => (
+					<select
+						key={'levels'}
+						value={attributes.levels}
+						onChange={ev => setAttributes( {levels: ev.target.value} )}
+					>
+						<option value="100">
+							{I18N.display.all}
+						</option>
+						{range( 1, 10 ).map( n => <option
+							key={n}
+							value={n}>{n}</option> )}
+					</select>
+				) )}
 
-			<Slot name="AdvancedSidebarMenuPagesDisplay" />
+			{CONFIG.blocks.pages.id === name &&
+				<Slot name="AdvancedSidebarMenuPagesDisplay" />}
+			{CONFIG.blocks.categories.id === name &&
+				<Slot name="AdvancedSidebarMenuCategoriesDisplay" />}
 
 		</PanelBody>
 	);
