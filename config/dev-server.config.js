1ef28a72e045eb7ec33d24322b3a58224e142665
lipemat <mat@matlipe.com>
2023-10-06 13:03:11 -0500

    Include everything in the PHPCS configuration

--- config/dev-server.config.js
+++ config/dev-server.config.js
@@ -1,5 +1,5 @@
-/**
- * Use a separate port for basic version, so we can run both plugin's
+/*
+ * Use a separate port for the basic version, so we can run both plugins
  * `start` script at the same time.
  *
  * @type {{post: number}}
