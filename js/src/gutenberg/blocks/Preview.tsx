339565da47b00f7273d714024ab8ce419ca12c42
lipemat <mat@matlipe.com>
2024-02-19 15:04:04 -0600

    Fix the missing dashicons shown in the Preview component inside iframes
    Now that the blocks are on apiVersion 3, they display inside an iframe which
    does not have dashicons available.
    
    Instead of enqueuing the dashicons font ourselves in the iframe, we use an
     SVG for this case.

--- js/src/gutenberg/blocks/Preview.tsx
+++ js/src/gutenberg/blocks/Preview.tsx
@@ -25,6 +25,17 @@ type Props<A> = {
 };
 
 /**
+ * Same as the Dashicons `welcome-widgets-menus` icon but available inside
+ * iframe editors which don't enqueue the Dashicons font.
+ */
+const ICON = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+	<rect x="0" fill="none" width="20" height="20" />
+	<g>
+		<path d="M19 16V3c0-.55-.45-1-1-1H3c-.55 0-1 .45-1 1v13c0 .55.45 1 1 1h15c.55 0 1-.45 1-1zM4 4h13v4H4V4zm1 1v2h3V5H5zm4 0v2h3V5H9zm4 0v2h3V5h-3zm-8.5 5c.28 0 .5.22.5.5s-.22.5-.5.5-.5-.22-.5-.5.22-.5.5-.5zM6 10h4v1H6v-1zm6 0h5v5h-5v-5zm-7.5 2c.28 0 .5.22.5.5s-.22.5-.5.5-.5-.22-.5-.5.22-.5.5-.5zM6 12h4v1H6v-1zm7 0v2h3v-2h-3zm-8.5 2c.28 0 .5.22.5.5s-.22.5-.5.5-.5-.22-.5-.5.22-.5.5-.5zM6 14h4v1H6v-1z" />
+	</g>
+</svg>;
+
+/**
  * Sanitize a client id for use as an HTML id.
  *
  * Must not start with a `-` or a digit.
@@ -62,21 +73,21 @@ const getSidebarId = ( clientId: string ): string => {
  */
 const Page = () => <Placeholder
 	className={styles.placeholder}
-	icon={'welcome-widgets-menus'}
+	icon={ICON}
 	label={__( 'Advanced Sidebar - Pages', 'advanced-sidebar-menu' )}
 	instructions={__( 'No preview available', 'advanced-sidebar-menu' )}
 />;
 
 const Category = () => <Placeholder
 	className={styles.placeholder}
-	icon={'welcome-widgets-menus'}
+	icon={ICON}
 	label={__( 'Advanced Sidebar - Categories', 'advanced-sidebar-menu' )}
 	instructions={__( 'No preview available', 'advanced-sidebar-menu' )}
 />;
 
 const Navigation = () => <Placeholder
 	className={styles.placeholder}
-	icon={'welcome-widgets-menus'}
+	icon={ICON}
 	label={__( 'Advanced Sidebar - Navigation', 'advanced-sidebar-menu' )}
 	instructions={__( 'No preview available', 'advanced-sidebar-menu' )}
 />;
