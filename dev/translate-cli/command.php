ba312606ea61dd991d95e678ac9476c998d41f49
lipemat <mat@matlipe.com>
2024-07-10 12:15:38 -0400

    Update translation generator commands to more modern structure
    - Support _x
    - Support _nx

--- dev/translate-cli/command.php
+++ dev/translate-cli/command.php
@@ -1,28 +1,97 @@
 <?php
+/**
+ * Extract translations from PO files and generate JSON files.
+ *
+ * @version 2.0.0
+ */
 
 use Gettext\Extractors\Po as PoExtractor;
 use Gettext\Generators\Jed;
+use Gettext\Translation;
 use Gettext\Translations;
 
 require __DIR__ . '/vendor/autoload.php';
 
 const DOMAIN = 'advanced-sidebar-menu';
+const VERSION = '2.0.0';
 const RELATIVE_PATH = 'js/dist/advanced-sidebar-menu-block-editor.js';
 const EXTENSIONS = [ '.ts', '.js', 'jsx', 'tsx' ];
 
-class LocalGenerator extends Jed {
+/**
+ * Jed file generator.
+ *
+ * Adds some more meta data to JED translation files than the default generator.
+ *
+ * @see \WP_CLI\I18n\JedGenerator
+ */
+class JedGenerator extends Jed {
 	/**
-	 * Wrap the translations with `local_data` so WP can read
-	 * them. Otherwise, same as default Jed.
-	 *
-	 * @see WP_CLI\I18n\JedGenerator
+	 * {@parentDoc}.
 	 */
 	public static function toString( Translations $translations, array $options = [] ) {
-		$message = parent::toString( $translations );
-		$object = json_decode( $message, true );
-		return json_encode( [
-			'locale_data' => $object,
-		] );
+		$options += static::$options;
+		$domain = $translations->getDomain() ?: 'messages';
+		$messages = static::buildMessages( $translations );
+
+		$configuration = [
+			'' => [
+				'domain'       => $domain,
+				'lang'         => $translations->getLanguage() ?: 'en',
+				'plural-forms' => $translations->getHeader( 'Plural-Forms' ) ?: 'nplurals=2; plural=(n != 1);',
+			],
+		];
+
+		$data = [
+			'translation-revision-date' => $translations->getHeader( 'PO-Revision-Date' ),
+			'source'                    => $options['source'],
+			'domain'                    => $domain,
+			'generator'                 => 'OnPoint Plugins/' . VERSION,
+			'locale_data'               => [
+				$domain => $configuration + $messages,
+			],
+		];
+
+		return json_encode( $data, $options['json'] );
+	}
+
+
+	/**
+	 * Generates an array with all translations.
+	 *
+	 * @param Translations $translations
+	 *
+	 * @return array
+	 */
+	public static function buildMessages( Translations $translations ) {
+		$plural_forms = $translations->getPluralForms();
+		$number_of_plurals = is_array( $plural_forms ) ? ( $plural_forms[0] - 1 ) : null;
+		$messages = [];
+		$context_glue = chr( 4 );
+
+		foreach ( $translations as $translation ) {
+			/** @var Translation $translation */
+
+			if ( $translation->isDisabled() ) {
+				continue;
+			}
+
+			$key = $translation->getOriginal();
+
+			if ( $translation->hasContext() ) {
+				$key = $translation->getContext() . $context_glue . $key;
+			}
+
+			if ( $translation->hasPluralTranslations( true ) ) {
+				$message = $translation->getPluralTranslations( $number_of_plurals );
+				array_unshift( $message, $translation->getTranslation() );
+			} else {
+				$message = [ $translation->getTranslation() ];
+			}
+
+			$messages[ $key ] = $message;
+		}
+
+		return $messages;
 	}
 }
 
@@ -61,7 +130,10 @@ function make_json( $source_file ) {
 	}
 
 	$destination_file = "../../languages/{$base_file_name}.json";
-	$success = LocalGenerator::toFile( $js_translations, $destination_file );
+	$success = JedGenerator::toFile( $js_translations, $destination_file, [
+		'source'    => basename( $source_file ),
+		'Generator' => 'OnPoint Plugins\/' . VERSION,
+	] );
 
 	if ( ! $success ) {
 		echo sprintf( 'Could not create %s file.' . "\n", basename( $destination_file ) );
