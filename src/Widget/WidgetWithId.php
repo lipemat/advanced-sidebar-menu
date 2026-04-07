b57943acff9d885db7d9c1b020e14919e0981475
lipemat <mat@matlipe.com>
2026-04-07 08:34:22 -0400

    Switch all uses of `WidgetWithId` interface to `Widget` in PRO version
    PRO now requires version 9.8.0 of basic which contains the combined interface.
    
    Steps 4 and 5 of the widget interface migration plan.

--- src/Widget/WidgetWithId.php
+++ src/Widget/WidgetWithId.php
@@ -10,18 +10,19 @@ namespace Advanced_Sidebar_Menu\Widget;
  * @author     OnPoint Plugins
  * @since      9.6.0
  *
- * @todo       Step 3 of the migration plan: Once the minimum basic version is 9.8.0+
+ * @todo       Step 5 of the migration plan: Once the minimum PRO version is 9.11.0+
  *
  * @internal   A temporary interface until we can complete the migration plan.
  *           1. ~~Remove the `WidgetId` interface by moving all rules to `Widget` once the minimum PRO version is 9.9.0+.~~
  *           2. ~~Make note in the PRO version the `WidgetWithId` interface is deprecated and uses may be removed at the next basic
  *           version~~.
- *           3. Remove uses from the PRO version in favor of the `Widget` interface only when required basic version is 9.8.0.
- *           4. Make note in the basic version the `WidgetWithId` interface is deprecated and uses may be removed at the next PRO version.
- *           5. Remove uses from the basic version in favor of the `Widget` interface only on the next PRO version is that of step 4.
+ *           3. ~~Remove uses from the PRO version in favor of the `Widget` interface only when required basic version is 9.8.0~~.
+ *           4. ~~Make note in the basic version the `WidgetWithId` interface is deprecated and uses may be removed at the next PRO
+ *           version.~~
+ *           5. Remove uses from the basic version in favor of the `Widget` interface when required PRO version is 9.11.0+.
  *
  *
- * @deprecated 9.8.0 use `Widget` instead.
+ * @deprecated 9.8.0 use `Widget` instead. Will be removed in
  *
  * @phpstan-template SETTINGS of array<string, string|int|bool|array<string, string>>
  * @phpstan-template DEFAULTS of array<key-of<SETTINGS>, int|string|array<string, string>>
